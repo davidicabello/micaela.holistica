171ed3fcced4d2eb53d807449a126c3564ebf2fe
davidlp90 <davidivan.eda@gmail.com>
2022-01-25 14:32:39 -0300

    init
    init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 
 
 AOS.init();
+
+function findGreaterOrEqual(a, b) {
+    if (a === b) {
+        return "a and b are equal";
+    }
+    else if (a > b) {
+        return "a is greater";
+    }
+    else {
+        return "b is greater";
+    }
+}
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal"
+        : (a > b) ? "a is greater"
+            : "b is greater";
+}
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
